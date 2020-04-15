781ede38f72ecaffd119db0032c8f8b8f213bf9d
just_ort <dr.v991149@gmail.com>
2020-04-15 12:34:51 +0300

    (refactor): новая бд

--- CPServerPart/DataLayer/DataContext.cs
+++ CPServerPart/DataLayer/DataContext.cs
@@ -8,17 +8,32 @@ namespace CPServerPart.DataLayer
     {
         public static DataContext Instance;
         
-        public DbSet<Product> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Check> Checks { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<DailyReport> DailyReports { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<ProductUnit> ProductUnits { get; set; }
         
         public DataContext()
         {
             Database.EnsureCreated();
             Instance = this;
+
+            Products.Add(new Product());
+            Checks.Add(new Check());
+            Customers.Add(new Customer());
+            Employees.Add(new Employee());
+            DailyReports.Add(new DailyReport());
+            ProductUnits.Add(new ProductUnit());
+            Shops.Add(new Shop());
+
         }
  
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=cpcd;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer("Server=localhost;Database=master;Trusted_Connection=True;");
         }
     }
 }
